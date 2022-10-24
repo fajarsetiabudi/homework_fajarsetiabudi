6ea63c25c70a601995e81401d1d9f407e7ae358d
fajarsetiabudi <fajarui86@gmail.com>
2022-10-24 22:54:00 +0700

    modified file

--- StudentApp.java
+++ StudentApp.java
@@ -2,8 +2,7 @@ public class StudentApp{
     public static void main(String[] args){
        
         var student = new Student("Fajar Setia Budi", 27);
-        student.name = "Fajar Setia Budi";
-        student.age = 27;
+        
 
         System.out.println(student.name);
         System.out.println(student.age);
